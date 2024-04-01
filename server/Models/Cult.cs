7452cc7dfbd137cb4281451ae7f00ee013242a42
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-01 09:48:40 -0600

    created cult class

---
+++ server/Models/Cult.cs
@@ -0,0 +1,9 @@
+namespace winter24_insta_cult.Models;
+
+public class Cult : RepoItem
+{
+  public string Name { get; set; }
+  public string Description { get; set; }
+  public string CoverImg { get; set; }
+  public string LeaderId { get; set; }
+}
---
+++ server/Models/RepoItem.cs
@@ -0,0 +1,8 @@
+namespace winter24_insta_cult.Models;
+
+public abstract class RepoItem
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+}
