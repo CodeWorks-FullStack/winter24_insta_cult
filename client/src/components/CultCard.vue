36300274024313c6da7712d74730d8c78d900c9c
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-01 12:10:30 -0600

    rendering cult card

---
+++ client/src/components/CultCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-slate">
+    {{ cult.name }}
+  </div>
+</template>
+
+
+<script>
+import { Cult } from '../models/Cult.js'
+
+
+export default {
+  props: {
+    cult: { type: Cult, required: true }
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.bg-slate {
+  background-color: #2F2C2C;
+}
+</style>
--- client/src/pages/CultsPage.vue
+++ client/src/pages/CultsPage.vue
@@ -6,9 +6,9 @@
       </div>
     </section>
 
-    <section class="row">
+    <section class="row justify-content-center">
       <div v-for="cult in cults" :key="cult.id" class="col-7">
-        {{ cult.name }}
+        <CultCard :cult="cult" />
       </div>
     </section>
   </div>
